a5b4f2ba4c85a5ae3dc2bb43b9a30960ccc8a13e
Анна Тебенькова <tebenkova100@icloud.com>
2026-03-02 11:15:44 +0800

    add dependencies, User entity, configure hibernate.properties

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,86 @@
+package org.example.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private int age;
+    @Column
+    private LocalDateTime created_at;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, int age, LocalDateTime created_at) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.created_at = created_at;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", created_at=" + created_at +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package org.example.service.impl;
+
+import org.example.dao.UserDao;
+
+public class UserServiceImpl implements UserDao {
+}
